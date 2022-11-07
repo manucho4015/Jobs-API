351e97fecce0dc92e31580d7b06e4ff6bd4b716f
Emmanuel Ochieng <eochieng16@gmail.com>
2022-11-07 15:15:25 +0300

    update codebase

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require("express-async-errors");
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
+// const rateLimiter = require("express-rate-limit");
 
 // Swagger
 const swaggerUI = require("swagger-ui-express");
@@ -34,12 +34,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send('<h1>Jobs API - Manucho</h1><a href="/api-docs">Documentation</a>');
