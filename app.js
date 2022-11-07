db6dc6acb53941360dd6b6da985f4ebdfef9507b
Emmanuel Ochieng <eochieng16@gmail.com>
2022-11-07 15:12:30 +0300

    update codebase

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
 
 // routes
-
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 
