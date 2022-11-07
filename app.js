45d5a8f584d248c8751ff947c889869382ceb6ea
Emmanuel Ochieng <eochieng16@gmail.com>
2022-11-07 14:00:50 +0300

    rename environment port

--- app.js
+++ app.js
@@ -1,67 +1,62 @@
-require('dotenv').config();
-require('express-async-errors');
-
+require("dotenv").config();
+require("express-async-errors");
 
 // extra security packages
-const helmet = require('helmet')
-const cors = require('cors')
-const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limit')
-
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 // Swagger
-const swaggerUI = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yaml')
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // connectDB
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
 
-const authenticateUser = require('./middleware/authentication')
+const authenticateUser = require("./middleware/authentication");
 
 // routes
-const authRouter = require('./routes/auth')
-const jobsRouter = require('./routes/jobs')
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-
-app.set('trust proxy', 1)
+app.set("trust proxy", 1);
 app.use(express.json());
-app.use(helmet())
-app.use(cors())
-app.use(xss())
-app.use(rateLimiter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-}))
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API - Manucho</h1><a href="/api-docs">Documentation</a>')
-})
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
-
-
-
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API - Manucho</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
