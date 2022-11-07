744b87c318c74baf96d18fdc2dceee149702517b
Emmanuel Ochieng <eochieng16@gmail.com>
2022-11-07 15:18:18 +0300

    update codebase

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
