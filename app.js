0efaf7577bcd0222a72028382828c3e62905477c
Emmanuel Ochieng <eochieng16@gmail.com>
2022-11-08 16:20:51 +0300

    update codebase

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const express = require("express");
 const app = express();
 
 // connectDB
-
 const connectDB = require("./db/connect");
 
 const authenticateUser = require("./middleware/authentication");
